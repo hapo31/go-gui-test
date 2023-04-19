0347792870c54866e3a7cfc3d1653651c490c8a4
happo31 <happo31@gmail.com>
2023-04-20 01:08:12 +0900

    fix: GUI修正

--- calculator/gui.go
+++ calculator/gui.go
@@ -8,7 +8,8 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-type Calculator struct {
+type CalculatorElement struct {
+	Calclator
 	ResultLabel     *widget.Label
 	NumberButtons   []*widget.Button
 	OperatorButtons []*widget.Button
@@ -20,47 +21,73 @@ type Calculator struct {
 
 type CalculatorWindow struct {
 	fyne.Window
-	*Calculator
+	*CalculatorElement
 }
 
 func NewCalculatorWindow(app fyne.App) *CalculatorWindow {
 	w := app.NewWindow("g-calc")
-	calc := NewCalculator()
+	calc := NewCalculatorElement()
 	w.Resize(fyne.NewSize(200, 30))
+	var n []fyne.CanvasObject
 	var o []fyne.CanvasObject
 	for _, v := range calc.NumberButtons {
-		o = append(o, v)
+		n = append(n, v)
 	}
 	for _, v := range calc.OperatorButtons {
 		o = append(o, v)
 	}
 
-	c := container.NewWithoutLayout(container.NewGridWrap(fyne.NewSize(10, 10), o...))
+	c := container.NewVBox(
+		calc.ResultLabel,
+		container.NewGridWithColumns(2,
+			container.NewGridWithColumns(3, n...),
+			container.NewGridWithColumns(3, o...),
+		))
 	w.SetContent(c)
 
 	return &CalculatorWindow{
-		Window: w, Calculator: calc,
+		Window: w, CalculatorElement: calc,
 	}
 }
 
-func NewCalculator() *Calculator {
+func NewCalculatorElement() *CalculatorElement {
 
-	calc := &Calculator{}
+	calc := &CalculatorElement{}
 
 	calc.numberChannel = make(chan string)
 	calc.operatorChannel = make(chan string)
 
-	for i := 0; i < 10; i = i + 1 {
-		calc.NumberButtons = append(calc.NumberButtons, widget.NewButton(strconv.Itoa(i), func() {
-			calc.numberChannel <- strconv.Itoa(i)
+	calc.Calclator = NewCalculator()
+
+	calc.ResultLabel = widget.NewLabel("test")
+
+	for i := 0; i < 10; i += 1 {
+		number := strconv.Itoa(i)
+		calc.NumberButtons = append(calc.NumberButtons, widget.NewButton(number, func() {
+			calc.numberChannel <- number
 		}))
 	}
 
 	for _, v := range []string{"C", "+", "-", "/", "*", "="} {
-		calc.OperatorButtons = append(calc.OperatorButtons, widget.NewButton(v, func() {
-			calc.operatorChannel <- v
+		operator := v
+		calc.OperatorButtons = append(calc.OperatorButtons, widget.NewButton(operator, func() {
+			calc.operatorChannel <- operator
 		}))
 	}
+
+	go func() {
+		for {
+			select {
+			case n := <-calc.numberChannel:
+				calc.Calclator.AddStr(n)
+			case o := <-calc.operatorChannel:
+				calc.Calclator.AddStr(o)
+			default:
+				continue
+			}
+			calc.ResultLabel.SetText(calc.Calclator.Buffer)
+		}
+	}()
 
 	return calc
 }
